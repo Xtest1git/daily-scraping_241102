25be68e985b72b30147fba902d348f94d084c79a
Xtest1git <tx8835954@gmail.com>
2024-11-04 23:35:11 +0900

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -10,9 +10,9 @@ def get_new_data():
     ]
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Referer": "https://movie.eroterest.net"
-    }
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",  # 最新のUser-Agentに変更
+    "Referer": "https://movie.eroterest.net"
+}
 
     data = []
 
